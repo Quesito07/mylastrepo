b29d6a781dd4d538af835f138caa1fd339543076
Edob <einerpro58@gmail.com>
2026-02-18 11:01:33 -0500

    primer releasing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mylastrepo",                           
+    version="0.1.0",                                
+    packages=find_packages(),                       
+    description="Un paquete pip simple de saludo",  
+    author="Einer Ospino",                         
+    author_email="einerpro58@gmail.com",                 
+    url="https://github.com/Quesito07/mylastrepo",     
+)
